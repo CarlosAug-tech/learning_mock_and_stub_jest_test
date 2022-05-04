248e78a3566f297263531a216ec42e1c4ba19a3c
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-05-04 15:58:32 -0300

    refactor: create sut factory

--- __tests__/application/modules/cars/usecases/create-category/create-category-usecase.spec.ts
+++ __tests__/application/modules/cars/usecases/create-category/create-category-usecase.spec.ts
@@ -1,24 +1,58 @@
-import { CategoriesRepositoryInMemory } from '@application/modules/cars/repositories/in-memory/categories-repository-in-memory';
+import {
+  ICreateCategoryRequestDTO,
+  ICreateCategoryResponseDTO,
+} from '@application/modules/cars/dtos/create-category-dtos';
+import { ICategoriesRepository } from '@application/modules/cars/repositories/contracts/categories-repository';
 import { CreateCategoryUseCase } from '@application/modules/cars/usecases/create-category/create-category-usecase';
 
-let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
-let createCategoryUseCase: CreateCategoryUseCase;
+const makeCategoriesRepositoryStub = (): ICategoriesRepository => {
+  class CategoriesRepositoryStub implements ICategoriesRepository {
+    create(
+      data: ICreateCategoryRequestDTO,
+    ): Promise<ICreateCategoryResponseDTO> {
+      const categoryFake = {
+        id: 'valid_id',
+        name: 'valid_category',
+        created_at: new Date(),
+      };
+
+      return new Promise(resolve => resolve(categoryFake));
+    }
+  }
+
+  return new CategoriesRepositoryStub();
+};
+
+interface ISutTypes {
+  sut: CreateCategoryUseCase;
+  categoriesRepositoryStub: ICategoriesRepository;
+}
+
+const makeSut = (): ISutTypes => {
+  const categoriesRepositoryStub = makeCategoriesRepositoryStub();
+  const sut = new CreateCategoryUseCase(categoriesRepositoryStub);
+
+  return {
+    sut,
+    categoriesRepositoryStub,
+  };
+};
 
 describe('Create Category UseCase', () => {
-  beforeAll(() => {
-    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
-    createCategoryUseCase = new CreateCategoryUseCase(
-      categoriesRepositoryInMemory,
-    );
-  });
-
   it('should be able to create a new Category', async () => {
+    const { sut } = makeSut();
+
     const category = {
       name: 'valid_category',
     };
 
-    const newCategory = await createCategoryUseCase.execute(category);
+    const newCategory = await sut.execute(category);
 
     expect(newCategory).toHaveProperty('id');
+    expect(newCategory).toEqual({
+      id: 'valid_id',
+      name: 'valid_category',
+      created_at: newCategory.created_at,
+    });
   });
 });
