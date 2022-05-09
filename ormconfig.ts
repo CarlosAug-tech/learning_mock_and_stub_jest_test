942a124b5fe56eb0543b2a4feee11bf3b35fa9f6
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-05-09 14:29:43 -0300

    feat: config database typeorm on application

---
+++ ormconfig.ts
@@ -0,0 +1,13 @@
+export = {
+  type: process.env.DB_TYPE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  migrations: ['./src/shared/infra/typeorm/migrations/*.ts'],
+  cli: {
+    migrationsDir: './src/infra/database/typeorm/migrations/',
+  },
+  entities: ['./src/infra/database/typeorm/entities/*.ts'],
+};
--- src/infra/http/app.ts
+++ src/infra/http/app.ts
@@ -1,6 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
+import 'reflect-metadata';
+
+import createConnection from '@infra/database/typeorm';
 import { routes } from './routes';
+
+createConnection();
 
 const app = express();
 
